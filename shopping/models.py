98f97cd10e357362f8ea1c10d825d5f57e4f35c5
Dan Sutich <dsutich@gmail.com>
2016-05-11 15:57:19 -0400

    Adding a bunch of templates for registration and some basic css/js

---
+++ shopping/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from datetime import date
+
+from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext as _
+
+class ShoppingList(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False)
+    label = models.CharField(max_length=255)
+    _date = models.DateField(default=date.today, verbose_name=_("date"))
+    items = models.ManyToManyField('ListItem', blank=True,
+                                    related_name='items')
+    
+    def __unicode__(self):
+        return "%s - %s" %(self.label, self._date)
+
+class ListItem(models.Model):
+    name = models.CharField(max_length=255)
+    purchased = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return self.name
